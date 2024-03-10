978a980124c32fdc4b250edc94be80f996a998e1
sametj <mosesjames2000@gmail.com>
2024-03-10 15:07:58 -0400

    MIGRATION

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,75 @@ import {
 	useScroll,
 	ScrollControls,
 	useHelper,
+	Text,
+	MeshReflectorMaterial,
 } from "@react-three/drei";
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef, forwardRef, useState } from "react";
 import { useFrame, useLoader, useThree } from "@react-three/fiber";
-import { forwardRef } from "react";
+
 import { gsap } from "gsap";
 import { useControls } from "leva";
-import Avatar from "./components/Avatar";
 import { Perf } from "r3f-perf";
-
 import * as THREE from "three";
-import Desk from "./components/Desk";
 
-function App() {
+import Desk from "./components/Desk";
+import MyAvatar from "./components/MyAvatar";
+
+export default function App() {
+	const [section, setSection] = useState(0);
+	const introSection = useRef();
+	const aboutSection = useRef();
+	const scroll = useScroll();
+
+	console.log(scroll.el);
+
 	return (
 		<>
 			<Perf />
 			<color
 				attach='background'
-				args={["#C0DEE5"]}
+				args={["#2c4767"]}
 			/>
-			<ambientLight intensity={1} />
-			<Stage>
-				<Center position={[0, 1.5, -1]}>
-					<Desk />
-				</Center>
-			</Stage>
+			<fog
+				attach='fog'
+				args={["orange", 0, 100]}
+			/>
+
+			<Scroll>
+				<group ref={introSection}>
+					<Text
+						font={"/fonts/kdam-thmor-pro-v4-latin-regular.woff"}
+						fontSize={0.2}
+						textAlign='center'
+						anchorX={"center"}
+						outlineWidth={0.01}
+						color={"OrangeRed"}
+						outlineColor={"black"}
+						outlineOffsetY={0.01}
+						outlineBlur={0.02}
+						outlineOpacity={0.3}
+						position={[0, 1.9, 0.5]}>
+						Hello!, Welcome to my portfolio.
+					</Text>
+
+					<Stage
+						shadows={"contact"}
+						preset={"portrait"}
+						adjustCamera={true}
+						environment={"city"}
+						intensity={2}>
+						<Desk
+							rotation-y={-Math.PI / 2}
+							position-z={5}
+							position-y={0}
+						/>
+					</Stage>
+				</group>
+
+				<group position={[0, -5, 0]}>
+					<MyAvatar scale={1} />
+				</group>
+			</Scroll>
 		</>
 	);
 }
-
-export default forwardRef(App);
--- src/index.jsx
+++ src/index.jsx
@@ -4,22 +4,30 @@ import App from "/App";
 import { Canvas } from "@react-three/fiber";
 
 import { ScrollControls } from "@react-three/drei";
+import { Camera } from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-
 const cameraSettings = {
-	fov: 45,
+	fov: 15,
 	near: 0.1,
-	far: 2000,
-	position: [0, 2, 8],
+	far: 1000,
+	position: [0, 1, 3],
 };
 
 root.render(
 	<>
-		<Canvas
-			dpr={[1, 2]}
-			camera={cameraSettings}>
-			<App />
+		<Canvas camera={cameraSettings}>
+			<ScrollControls
+				eps={1}
+				pages={8}
+				damping={0.1}
+				maxSpeed={10}
+				distance={1}
+				style={{
+					height: "100vh",
+				}}>
+				<App />
+			</ScrollControls>
 		</Canvas>
 	</>
 );
